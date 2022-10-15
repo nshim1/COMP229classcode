342bbfa48778c8e4b60fa6300565441c2be16cb4
Nayeon Shim <nshim1@my.centennialcollege.ca>
2022-10-14 23:36:31 -0400

    function type

--- ArrowFunction/index.js
+++ ArrowFunction/index.js
@@ -1,12 +1,27 @@
-function hello() {
-  document.getElementById("demo").innerHTML += "Hello world!";
-}
-
-function hello() {
-  document.getElementById("demo").innerHTML += "Hello world!";
-}
-
 var hello;
-hello = function () {
-  document.getElementById("demo").innerHTML += "Hello world!";
+
+//Arrow Function
+hello = () => {
+  document.getElementById("demo").innerHTML += "Hello 1!";
 };
+
+//Anounymous function
+var hello2 = function () {
+  document.getElementById("demo").innerHTML += "Hello 2!";
+};
+
+//Named Function
+function hello3() {
+  document.getElementById("demo").innerHTML += "Hello 3!";
+}
+//The window object calls the function
+window.addEventListener("load", hello);
+
+//A button object calls the function:
+document.getElementById("btn1").addEventListener("click", function () {
+  document.getElementById("demo").innerHTML += "Hello 1!";
+});
+
+document.getElementById("btn2").addEventListener("click", () => {
+  document.getElementById("demo").innerHTML += "Hello2!";
+});
