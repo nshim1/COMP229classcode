54de312a12c2907863ac7605ad09bc5bd86f9e6a
Nayeon Shim <nshim1@my.centennialcollege.ca>
2022-10-15 00:02:01 -0400

    let/var and const

---
+++ LetAndConst/index.js
@@ -0,0 +1,24 @@
+function iterateVar() {
+  for (var i = 0; i < 10; i++) {
+    console.log(i);
+  }
+  console.log(i);
+}
+
+function iterateLet() {
+  for (let i = 0; i < 10; i++) {
+    console.log(i);
+  }
+  console.log(i); // unable to print 'i'
+}
+
+iterateVar();
+iterateLet();
+
+function reinitConst() {
+  const me = 1;
+  console.log(me);
+  me = 2;
+  console.log(me); // error
+}
+reinitConst();
