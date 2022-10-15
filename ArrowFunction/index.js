965d9bd39496f6419fc4b5a171b7958ce2b252c2
Nayeon Shim <nshim1@my.centennialcollege.ca>
2022-10-14 23:47:59 -0400

    object

--- ArrowFunction/index.js
+++ ArrowFunction/index.js
@@ -26,6 +26,8 @@ document.getElementById("btn2").addEventListener("click", () => {
   document.getElementById("demo").innerHTML += "Hello2!";
 });
 
+// =============================================
+
 var numbers = [1, 2, 3, 4, 5];
 const squares = numbers.map((n) => n * n); // for each any inside of the numeber run n*n
 document.write(squares);
@@ -38,3 +40,36 @@ numbers.forEach((n) => {
 });
 document.write("<br>");
 document.write(evens);
+
+// =============================================
+
+const author = {
+  fullName: "Bob Alice",
+  books: ["Book 01", "Book 02", "Book 03"],
+
+  printBooks() {
+    this.books.forEach(
+      (book) => document.write(book + " by " + this.fullName + "<br>")
+      // this -> author object
+    );
+  },
+
+  printBooks1() {
+    function printValue(book) {
+      document.write(book + " by " + this.fullName + "<br>"); // Undefined expected for fullname
+    }
+    this.books.forEach(printValue);
+  },
+
+  printBooks2() {
+    this.books.forEach(function (book) {
+      document.write(book + " by " + this.fullName + "<br>");
+      // Undefined expected for fullname
+      // scope of this -> inside of current function
+    });
+  },
+};
+
+author.printBooks();
+author.printBooks1();
+author.printBooks2();
