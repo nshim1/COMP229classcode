1e5e6149577a3b89b8c4334885495951c981d32c
Nayeon Shim <nshim1@my.centennialcollege.ca>
2022-10-14 23:21:33 -0400

    arrow function

---
+++ ArrowFunction/index.js
@@ -0,0 +1,12 @@
+function hello() {
+  document.getElementById("demo").innerHTML += "Hello world!";
+}
+
+function hello() {
+  document.getElementById("demo").innerHTML += "Hello world!";
+}
+
+var hello;
+hello = function () {
+  document.getElementById("demo").innerHTML += "Hello world!";
+};
